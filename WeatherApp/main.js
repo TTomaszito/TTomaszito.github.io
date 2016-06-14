0314acb92fb8bfadb6fc7309843f5d7df64ebe0c
Ttomaszito <TTomaszito>
2016-06-14 17:10:22 -0400

    Updated files

--- WeatherApp/main.js
+++ WeatherApp/main.js
@@ -1,46 +1,82 @@
 $(document).ready(function() {
 
-  var latitude, longitude, neighborhood, city, state, temperature,summary, url;
-  
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      latitude = position.coords.latitude;
-      longitude = position.coords.longitude;
+  var latitude, longitude, neighborhood, city, state, temperature, summary, icon, url;
 
-      var GEOCODING = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + '%2C' + longitude + '&language=en';
+  navigator.geolocation.getCurrentPosition(function(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
 
-      $.getJSON(GEOCODING).done(function(location) {
-        neighborhood = location.results[1].address_components[0].long_name;
-        city = location.results[1].address_components[1].long_name;
-        state = location.results[1].address_components[4].short_name;
-        
-        $("#locationDisplay").html(neighborhood+" ,"+city+" ,"+state);
+    var GEOCODING = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + '%2C' + longitude + '&language=en';
 
-        console.log(neighborhood,city ,state);
-        url = "https://api.forecast.io/forecast/a6a19b73560bb483d6ebdb9fdcc0b8c2/" + latitude + "," + longitude;
+    $.getJSON(GEOCODING).done(function(location) {
+      neighborhood = location.results[1].address_components[0].long_name;
+      city = location.results[1].address_components[1].long_name;
+      state = location.results[1].address_components[4].short_name;
 
-          $.ajax({
+      $("#locationDisplay").html(neighborhood + " ," + city + " ," + state);
 
-            url: url,
-            dataType: 'jsonp',
-            success: function(results) {
-              
-            temperature = results.currently.temperature;
-              $("#temperatureDisplay").html(temperature);
-            summary = results.currently.summary;
-              $("#summaryDisplay").html(summary);
-              
-              console.log(results)
-              
-              
-            }
-          });
+      //console.log(neighborhood,city ,state);
+      url = "https://api.forecast.io/forecast/a6a19b73560bb483d6ebdb9fdcc0b8c2/" + latitude + "," + longitude;
 
+      $.ajax({
+
+        url: url,
+        dataType: 'jsonp',
+        success: function(results) {
+
+          temperature = results.currently.temperature;
+          $("#temperatureDisplay").html(temperature);
+          summary = results.currently.summary;
+          $("#summaryDisplay").html(summary);
+          icon = results.currently.icon;
+          //$("#iconDisplay").html(icon);
+          statusIcon(icon);
+
+        }
       });
 
     });
-  };
+
+  });
+
 });
+
+function statusIcon(icon) {
+
+  switch (icon) {
+    case "partly-cloudy-day":
+      $("#iconDisplay").html('<i class="wi wi-day-cloudy"></i>');
+      break;
+    case "partly-cloudy-night":
+      $("#iconDisplay").html('<i class="wi wi-night-alt-cloudy"></i>');
+      break;
+    case "clear-day":
+      $("#iconDisplay").html('<i class="wi wi-day-sunny "></i>');
+      break;
+    case "clear-night":
+      $("#iconDisplay").html('<i class="wi wi-night-clear"></i>');
+      break;
+    case "rain":
+      $("#iconDisplay").html('<i class="wi wi-rain"></i>');
+      break;
+    case "snow":
+      $("#iconDisplay").html('<i class="wi wi-snow"></i>');
+      break;
+    case "sleet":
+      $("#iconDisplay").html('<i class="wi wi-sleet"></i>');
+      break;
+    case "wind":
+      $("#iconDisplay").html('<i class="wi wi-cloudy-windy"></i>');
+      break;
+    case "fog":
+      $("#iconDisplay").html('<i class="wi wi-fog"></i>');
+      break;
+    case "cloudy":
+      $("#iconDisplay").html('<i class="wi wi-cloudy"></i>');
+      break;
+  };
+
+};
 
 // alternate methood to geolocation to be designed later.
 
