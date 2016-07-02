09400738eea1df6af006a42463580a1dfe505b8d
Ttomaszito <TTomaszito>
2016-07-02 19:27:08 -0400

    updated functionality

--- WikipediaViewer/main.js
+++ WikipediaViewer/main.js
@@ -4,27 +4,36 @@ $(document).ready(function() {
   $("#random_article").on("click", function() {
     var link = "https://en.wikipedia.org/wiki/Special:Random"
     window.open(link);
-  });
-  
-  var search_term = "intel";
-  var results_number = "5";
-  
-  var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+"%22+"+ 
-      search_term +"+%22&limit="+
-      results_number +"&format=json&callback=?";
-  
-  $.ajax({
 
-    url: url,
-    dataType: 'jsonp',
-    type: 'POST',
-    headers: {
-      'Wikiviewer': 'myviewer1'
-    },
-    success: function(results) {
-      console.log(results)
-
-    }
   });
+
+  $('form').submit(
+    function(event) {
+      event.preventDefault();
+      var search_term= $("#Search_term").val();
+      console.log(search_term)
+
+      
+      var results_number = "5";
+
+      var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + "%22+" +
+        search_term + "+%22&limit=" +
+        results_number + "&format=json&callback=?";
+
+      $.ajax({
+
+        url: url,
+        dataType: 'jsonp',
+        type: 'POST',
+        headers: {
+          'Wikiviewer': 'myviewer1'
+        },
+        success: function(results) {
+          console.log(results)
+
+        }
+      });
+    });
 
 });
+
