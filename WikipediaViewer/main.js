bcb64692148a7e7605a1704b27fe1f0327c61d4a
Ttomaszito <TTomaszito>
2016-07-02 17:33:24 -0400

    added wiki search function

--- WikipediaViewer/main.js
+++ WikipediaViewer/main.js
@@ -1,11 +1,30 @@
 $(document).ready(function() {
-  
- // Fetch random wikipedia Article 
+
+  // Fetch random wikipedia Article 
   $("#random_article").on("click", function() {
     var link = "https://en.wikipedia.org/wiki/Special:Random"
     window.open(link);
   });
   
+  var search_term = "intel";
+  var results_number = "5";
   
+  var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+"%22+"+ 
+      search_term +"+%22&limit="+
+      results_number +"&format=json&callback=?";
   
+  $.ajax({
+
+    url: url,
+    dataType: 'jsonp',
+    type: 'POST',
+    headers: {
+      'Wikiviewer': 'myviewer1'
+    },
+    success: function(results) {
+      console.log(results)
+
+    }
+  });
+
 });
