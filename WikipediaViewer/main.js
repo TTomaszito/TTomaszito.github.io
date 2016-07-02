48877a3c6484b878858c99f2acd2bedaf3eff69a
Ttomaszito <TTomaszito>
2016-07-02 15:24:56 -0400

    new project

---
+++ WikipediaViewer/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  
+ // Fetch random wikipedia Article 
+  $("#random_article").on("click", function() {
+    var link = "https://en.wikipedia.org/wiki/Special:Random"
+    window.open(link);
+  });
+  
+  
+  
+});
