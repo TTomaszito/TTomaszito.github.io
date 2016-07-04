c3294b8a307c06512e4cd5788e4cccf74a01e2b3
Ttomaszito <TTomaszito>
2016-07-04 16:39:16 -0400

    added number of articles functionality

--- WikipediaViewer/main.js
+++ WikipediaViewer/main.js
@@ -1,54 +1,57 @@
 $(document).ready(function() {
 
-  // Fetch random wikipedia Article 
-  $("#random_article").on("click", function() {
-    var link = "https://en.wikipedia.org/wiki/Special:Random"
-    window.open(link);
+  	// Fetch random wikipedia Article 
+  	$("#RandomArticle").on("click", function() {
+  		var link = "https://en.wikipedia.org/wiki/Special:Random"
+  		window.open(link);
 
-  });
+  	});
 
   // search keyword return results
 
-  $('#target').submit(function(event) {
-    event.preventDefault();
+  	$('#target').submit(function(event) {
+	  	event.preventDefault();
 
-    var search_term = $("#Search_term").val();
-    var results_number = "5";
+	  	var search_term = $("#SearchTerm").val();
+	  	var results_number = $("#SearchNumber").val();
 
-    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + "%22+" +
-      search_term + "+%22&limit=" +
-      results_number + "&format=json&callback=?";
+	  	console.log(search_term,results_number);
 
-    $.ajax({
+	  	var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + "%22+" +
+	  	search_term + "+%22&limit=" +
+	  	results_number + "&format=json&callback=?";
 
-      url: url,
-      dataType: 'jsonp',
-      type: 'POST',
-      
-      success: function(results) {
+	  	$.ajax({
 
-        $("#display_articles").empty(); // empty's the div on every new search.
+	  		url: url,
+	  		dataType: 'jsonp',
+	  		type: 'POST',
 
-        for (i = 0; i < results_number; i++) {
-          var title = results[1][i];
-          var summary = results[2][i];
-          var link = results[3][i];
-          console.log(title)
+	  		success: function(results) {
 
-          $("#display_articles").append('<div class="title">' + title + '</div>');
-          $("#display_articles").append('<div class="summary">'+ summary + '</div>');
-          $("#display_articles").append('<div class="link">'+ link + '</div>');
-          
-          if (title == undefined){
-            
-            $("#display_articles").empty();
-          };
-    
-        };
+	        	$("#display_articles").empty(); // empty's the div on every new search.
+
+	        	for (i = 0; i < results_number; i++) {
+		        	var title = results[1][i];
+		        	var summary = results[2][i];
+		        	var link = results[3][i];
+		        	console.log(title)
+
+		        	$("#display_articles").append('<div class="title">' + title + '</div>');
+		        	$("#display_articles").append('<div class="summary">'+ summary + '</div>');
+		        	$("#display_articles").append('<div class="link">'+ link + '</div>');
+
+		        	if (title == undefined){
+
+		        		$("#display_articles").empty();
+		        	};
+
+	        	};
 
 
-      }
-    });
-  });
+    		}
+		});
+
+  	});
 
 });
