7f0848d6e35b61211186beb7fd4980e5aaab222d
Ttomaszito <TTomaszito@gmail.com>
2016-07-27 21:54:05 -0400

    created Get data function

--- TwitchTv/main.js
+++ TwitchTv/main.js
@@ -1,18 +1,29 @@
 $( document ).ready(function() {
 
+	var List = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
+
     console.log( "ready!" );
+
+    List.forEach(function logArrayElements (element, index, a) {
+    	console.log('a[' + index + '] = ' + element);
+    	
+    });
+
+
     Get_Data();
 
 
 });
 
 function Get_Data(){
+	
 	console.log("function ready");
 
-	$.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp?callback=?', function(data) {
+	$.getJSON('https://api.twitch.tv/kraken/channels/ESL_SC2', function(data) {
   		console.log(data);
 	});
 
 
 
 };
+
