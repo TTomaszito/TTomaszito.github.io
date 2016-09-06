9a80735cbfe200dfae7bc3fd3cd06820b48fd2e6
Ttomaszito <TTomaszito@gmail.com>
2016-09-05 23:44:47 -0400

    organized code

---
+++ TwitchTv/contentcreate.js
@@ -0,0 +1,104 @@
+
+
+
+function CloseVideo(){
+
+	$("#Off_Video_Button").on("click",function(){
+
+   		$("#video").empty();
+   		$("#video_controls").hide();
+
+   	});
+};
+
+
+
+function LightsOff(){
+	$("#Cinema_Mode").on("click",function(event){
+   		event.stopPropagation();
+
+   		$("#Lights_Off").toggle(true);
+
+   	});
+   	$("body").on('click', function (event) {
+
+		$("#Lights_Off").toggle(false);
+	});
+};
+
+
+function ShowVideo(channel){
+
+	var Livestream = $('<iframe class="livestream" src="https://www.twitch.tv/'+ 
+	channel +'/embed" frameborder="0" scrolling="no" height="720" width="1080" align="middle"></iframe>');
+	
+	event.preventDefault();
+
+	$("#video").empty();
+
+	$("#video").append(
+
+		"<div class=" + "video-container" +" "+"id="+"video_stream" + "></div>"
+
+	);
+
+	$("#video_controls").show();
+
+
+	$("#video_stream").append(Livestream);
+
+};
+
+
+function LogoUnavailable(Left_channel_div){
+
+	$(Left_channel_div).append(
+
+		"<img class="+"img-responsive center-block"+" "+"style=" + "height:100px;" +" "+ "src="+ 
+		"http://s.jtvnw.net/jtv_user_pictures/hosted_images/GlitchIcon_PurpleonWhite.png" +">" 
+
+	);
+};
+
+function LogoAvailable(Left_channel_div,data){
+	$(Left_channel_div).append(
+
+		"<img class="+"img-responsive center-block"+" "+"style=" + "height:100px;" +" "+ "src="+ 
+		data.stream.channel.logo +">"  
+
+	);
+};
+
+function AppendStructure(channel,channel_button,channel_div,Center_channel_div,Right_channel_div){
+	$("#Results").append(
+
+    	"<div" +" "+ "class=row"+" "+"id=" + channel + ">" +  "</div>"
+
+    );
+
+    $(channel_div).addClass("channel-div");
+
+
+
+    $(channel_div).append(
+
+    	"<div class=" + "col-xs-4" +" "+"id=Left"+ channel+ ">" + "</div>"+
+    	"<div class=" + "col-xs-4" +" "+"id=Center" + channel+">" + "</div>"+
+    	"<div class=" + "col-xs-4" +" "+"id=Right"+ channel+ ">" + "</div>"
+
+    );
+
+    $(Right_channel_div).append(
+
+    	"<button id=" + channel +"-btn" + ">"+"Watch" +"</button>"
+
+    );
+    $(channel_button).addClass("channel_Button");
+
+
+    $(Center_channel_div).append(
+
+    	"<p id=" + channel +"-name" + " " +"style=" + "padding-top:35px;padding-bottom:35px;"+">"+ channel.toString() +"</p>"
+
+    );
+};
--- TwitchTv/main.js
+++ TwitchTv/main.js
@@ -52,105 +52,29 @@ $( document ).ready(
 			    var channel_button = "#"+channel + "-btn"
 			    
 
-			    $("#Results").append(
+			    AppendStructure(channel,channel_button,channel_div,Center_channel_div,Right_channel_div);
 
-			    	"<div" +" "+ "class=row"+" "+"id=" + channel + ">" +  "</div>"
-
-			    );
-
-			    $(channel_div).addClass("channel-div");
-
-
-
-			    $(channel_div).append(
-
-			    	"<div class=" + "col-xs-4" +" "+"id=Left"+ channel+ ">" + "</div>"+
-			    	"<div class=" + "col-xs-4" +" "+"id=Center" + channel+">" + "</div>"+
-			    	"<div class=" + "col-xs-4" +" "+"id=Right"+ channel+ ">" + "</div>"
-
-			    );
-
-			    $(Right_channel_div).append(
-
-			    	"<button id=" + channel +"-btn" + ">"+"Watch" +"</button>"
-
-			    );
-			    $(channel_button).addClass("channel_Button");
-
-
-			    $(Center_channel_div).append(
-
-			    	"<p id=" + channel +"-name" + " " +"style=" + "padding-top:35px;padding-bottom:35px;"+">"+ channel.toString() +"</p>"
-
-			    );
-
-
-	 			
 
 	 			if(data.stream == null){
 	 				console.log("no data")
 
-	 				$(Left_channel_div).append(
-
-			    		"<img class="+"img-responsive center-block"+" "+"style=" + "height:100px;" +" "+ "src="+ 
-			    		"http://s.jtvnw.net/jtv_user_pictures/hosted_images/GlitchIcon_PurpleonWhite.png" +">" 
-
-			    	);
+	 				LogoUnavailable(Left_channel_div);
 	 			}
 	 				 
 	 			else{
 	 				console.log(data.stream.channel.logo)
 
-	 				$(Left_channel_div).append(
-
-			    		"<img class="+"img-responsive center-block"+" "+"style=" + "height:100px;" +" "+ "src="+ 
-			    		data.stream.channel.logo +">"  
-
-			    	);
+	 				LogoAvailable(Left_channel_div,data)
 	 			};
 
 
 			   	$(channel_button).click(function(event){
 
-			   		var Livestream = $('<iframe class="livestream" src="https://www.twitch.tv/'+ 
-					channel +'/embed" frameborder="0" scrolling="no" height="720" width="1080" align="middle"></iframe>');
-					
-					event.preventDefault();
-
-					$("#video").empty();
-
-					$("#video").append(
-
-			    		"<div class=" + "video-container" +" "+"id="+"video_stream" + "></div>"
-
-			    	);
-
-			    	$("#video_controls").show();
-
-
-					$("#video_stream").append(Livestream);
-
-					
+			   		ShowVideo(channel);
 				});
 
-
-			   	$("#Off_Video_Button").on("click",function(){
-
-			   		$("#video").empty();
-			   		$("#video_controls").hide();
-
-			   	});
-
-			   	$("#Cinema_Mode").on("click",function(event){
-			   		event.stopPropagation();
-
-			   		$("#Lights_Off").toggle(true);
-
-			   	});
-			   	$("body").on('click', function (event) {
-
-    				$("#Lights_Off").toggle(false);
-				});
+			   	CloseVideo();
+			   	LightsOff();
 				
  
 			});
@@ -177,107 +101,31 @@ $( document ).ready(
 
 			    var channel_button = "#"+channel + "-btn"
 			    
-			    if(data.stream !== null){
+			    if(data.stream !== null){			    
 
-				   	$("#Results").append(
+				    AppendStructure(channel,channel_button,channel_div,Center_channel_div,Right_channel_div);
 
-				    	"<div" +" "+ "class=row"+" "+"id=" + channel + ">" +  "</div>"
-
-				    );
-
-				    $(channel_div).addClass("channel-div");
-
-
-
-				    $(channel_div).append(
-
-				    	"<div class=" + "col-xs-4" +" "+"id=Left"+ channel+ ">" + "</div>"+
-				    	"<div class=" + "col-xs-4" +" "+"id=Center" + channel+">" + "</div>"+
-				    	"<div class=" + "col-xs-4" +" "+"id=Right"+ channel+ ">" + "</div>"
-
-				    );
-
-				    $(Right_channel_div).append(
-
-				    	"<button id=" + channel +"-btn" + ">"+"Watch" +"</button>"
-
-				    );
-				    $(channel_button).addClass("channel_Button");
-
-
-				    $(Center_channel_div).append(
-
-				    	"<p id=" + channel +"-name" + " " +"style=" + "padding-top:35px;padding-bottom:35px;"+">"+ channel.toString() +"</p>"
-
-				    );
-
-
-		 			
 
 		 			if(data.stream == null){
 		 				console.log("no data")
 
-		 				$(Left_channel_div).append(
-
-				    		"<img class="+"img-responsive center-block"+" "+"style=" + "height:100px;" +" "+ "src="+ 
-				    		"http://s.jtvnw.net/jtv_user_pictures/hosted_images/GlitchIcon_PurpleonWhite.png" +">" 
-
-				    	);
+		 				LogoUnavailable(Left_channel_div);
 		 			}
 		 				 
 		 			else{
 		 				console.log(data.stream.channel.logo)
 
-		 				$(Left_channel_div).append(
-
-				    		"<img class="+"img-responsive center-block"+" "+"style=" + "height:100px;" +" "+ "src="+ 
-				    		data.stream.channel.logo +">"  
-
-				    	);
+		 				LogoAvailable(Left_channel_div,data)
 		 			};
 
 
 				   	$(channel_button).click(function(event){
 
-				   		var Livestream = $('<iframe class="livestream" src="https://www.twitch.tv/'+ 
-						channel +'/embed" frameborder="0" scrolling="no" height="720" width="1080" align="middle"></iframe>');
-						
-						event.preventDefault();
-
-						$("#video").empty();
-
-						$("#video").append(
-
-				    		"<div class=" + "video-container" +" "+"id="+"video_stream" + "></div>"
-
-				    	);
-
-				    	$("#video_controls").show();
-
-
-						$("#video_stream").append(Livestream);
-
-						
+				   		ShowVideo(channel);
 					});
 
-
-				   	$("#Off_Video_Button").on("click",function(){
-
-				   		$("#video").empty();
-				   		$("#video_controls").hide();
-
-				   	});
-
-				   	$("#Cinema_Mode").on("click",function(event){
-				   		event.stopPropagation();
-
-				   		$("#Lights_Off").toggle(true);
-
-				   	});
-				   	$("body").on('click', function (event) {
-
-	    				$("#Lights_Off").toggle(false);
-					});
+				   	CloseVideo();
+				   	LightsOff();
 
 				    
 				};
@@ -308,105 +156,29 @@ $( document ).ready(
 			    
 			    if (data.stream == null) {
 
-					$("#Results").append(
+					AppendStructure(channel,channel_button,channel_div,Center_channel_div,Right_channel_div);
 
-				    	"<div" +" "+ "class=row"+" "+"id=" + channel + ">" +  "</div>"
-
-				    );
-
-				    $(channel_div).addClass("channel-div");
-
-
-
-				    $(channel_div).append(
-
-				    	"<div class=" + "col-xs-4" +" "+"id=Left"+ channel+ ">" + "</div>"+
-				    	"<div class=" + "col-xs-4" +" "+"id=Center" + channel+">" + "</div>"+
-				    	"<div class=" + "col-xs-4" +" "+"id=Right"+ channel+ ">" + "</div>"
-
-				    );
-
-				    $(Right_channel_div).append(
-
-				    	"<button id=" + channel +"-btn" + ">"+"Watch" +"</button>"
-
-				    );
-				    $(channel_button).addClass("channel_Button");
-
-
-				    $(Center_channel_div).append(
-
-				    	"<p id=" + channel +"-name" + " " +"style=" + "padding-top:35px;padding-bottom:35px;"+">"+ channel.toString() +"</p>"
-
-				    );
-
-
-		 			
 
 		 			if(data.stream == null){
 		 				console.log("no data")
 
-		 				$(Left_channel_div).append(
-
-				    		"<img class="+"img-responsive center-block"+" "+"style=" + "height:100px;" +" "+ "src="+ 
-				    		"http://s.jtvnw.net/jtv_user_pictures/hosted_images/GlitchIcon_PurpleonWhite.png" +">" 
-
-				    	);
+		 				LogoUnavailable(Left_channel_div);
 		 			}
 		 				 
 		 			else{
 		 				console.log(data.stream.channel.logo)
 
-		 				$(Left_channel_div).append(
-
-				    		"<img class="+"img-responsive center-block"+" "+"style=" + "height:100px;" +" "+ "src="+ 
-				    		data.stream.channel.logo +">"  
-
-				    	);
+		 				LogoAvailable(Left_channel_div,data)
 		 			};
 
 
 				   	$(channel_button).click(function(event){
 
-				   		var Livestream = $('<iframe class="livestream" src="https://www.twitch.tv/'+ 
-						channel +'/embed" frameborder="0" scrolling="no" height="720" width="1080" align="middle"></iframe>');
-						
-						event.preventDefault();
-
-						$("#video").empty();
-
-						$("#video").append(
-
-				    		"<div class=" + "video-container" +" "+"id="+"video_stream" + "></div>"
-
-				    	);
-
-				    	$("#video_controls").show();
-
-
-						$("#video_stream").append(Livestream);
-
-						
+				   		ShowVideo(channel);
 					});
 
-
-				   	$("#Off_Video_Button").on("click",function(){
-
-				   		$("#video").empty();
-				   		$("#video_controls").hide();
-
-				   	});
-
-				   	$("#Cinema_Mode").on("click",function(event){
-				   		event.stopPropagation();
-
-				   		$("#Lights_Off").toggle(true);
-
-				   	});
-				   	$("body").on('click', function (event) {
-
-	    				$("#Lights_Off").toggle(false);
-					});
+				   	CloseVideo();
+				   	LightsOff();
 				
 				};
  
